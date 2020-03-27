42dfa34732510fac757e4e066843c0b128825c29
linhhm2406 <linh.hm24@gmail.com>
2020-03-28 00:12:19 +0700

    add file

---
+++ src/Circle.java
@@ -0,0 +1,23 @@
+public class Circle {
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle with Radius : " + radius;
+    }
+}
